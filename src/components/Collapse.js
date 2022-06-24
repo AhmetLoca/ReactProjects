7c74fa2948efc73c030b6b78118c78b1cff60009
ahmetloca <locaahmet@gmail.com>
2022-06-24 12:44:43 +0300

    component lifecycle

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,35 +1,29 @@
-// -> ilk constructor -> state initialization
-// render metodu ilk defa calıstı.
-//onClick event'i calıstı.(showMore metodu calıstı)
-//showMore metodu setState yardımıyla bizim state'imizi update etti.
-//setState metodu render() metodunu trigger ediyor !
-//son olarak render metodu güncel JSX'i sayfada gösteriyor.
-
 import React from 'react';
 class Collapse extends React.Component {
   constructor() {
     super();
-
     this.state = {
       showContent: false
     }
   }
-
   showMore = () => {
     this.setState({ showContent: !this.state.showContent })
-
   }
 
   render() {
     return (
       <div>
         <button className="btn btn-primary w-100" onClick={this.showMore}>
-          Link with href
+          {/* {this.props.children.props.cardTitle} */}
+          {React.Children.map(this.props.children, children => children.props.cardTitle)}
+
+
         </button>
         {
           this.state.showContent ? (
             <div className="collapse show">
-              {this.props.children}
+              {/* {this.props.children} */}
+              {React.Children.map(this.props.children, children => children)}
             </div>
           ) : null
         }

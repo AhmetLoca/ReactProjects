351b27c069de7f9b1378d9736624148fca6514cd
ahmetloca <locaahmet@gmail.com>
2022-06-25 12:04:48 +0300

    fake rest api,fetch,async-await

--- my-movies/src/components/App.js
+++ my-movies/src/components/App.js
@@ -1,63 +1,23 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
-
-// movies bilgisini bir state içerisinde tanımlamamız gerek. State kullandıgımız için fonksiyonel component'imi class base bi component'ta cevirecegim.
-
 class App extends React.Component {
 
   state = {
-    movies: [
-      {
-        "id": 1,
-        "name": "The Flash",
-        "rating": 8.3,
-        "overview": "1",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-      },
-
-      {
-        "id": 2,
-        "name": "Interstellar",
-        "rating": 6.3,
-        "overview": "2",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-      },
-
-      {
-        "id": 3,
-        "name": "Arrow",
-        "rating": 7.9,
-        "overview": "3",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-      },
-
-      {
-        "id": 4,
-        "name": "Rogue",
-        "rating": 7.4,
-        "overview": "4",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
-      },
-
-      {
-        "id": 5,
-        "name": "Project Power",
-        "rating": 6.9,
-        "overview": "5",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
-      },
-
-      {
-        "id": 6,
-        "name": "Superman",
-        "rating": 7.6,
-        "overview": "6",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
-      }
-    ],
+    movies: [],
     searchQuery: ""
   }
+
+  async componentDidMount() {
+    const baseURL = "http://localhost:3002/movies";
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    this.setState({ movies: data })
+
+  }
+  //dinamik olarak json serverdan aldıgımız veriyi setState metoduyla state'imizi güncelledik.
+
+  //artık verileri movies.json dosyasından geliyor.
 
   deleteMovie = (movie) => {
     const newMovieList = this.state.movies.filter(
@@ -71,6 +31,8 @@ class App extends React.Component {
   searchMovie = (event) => {
     this.setState({ searchQuery: event.target.value })
   }
+
+
 
   render() {
 

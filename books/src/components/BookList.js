90db964016f0c0f24d89f3c59980471d4c8739f0
ahmetloca <locaahmet@gmail.com>
2022-06-26 21:44:58 +0300

    context api, this.context yöntemi

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import Book from './Book';
+import './BookList.css';
+import BookContext from '../contexts/BookContexts';
 
 class BookList extends React.Component {
 
+  static contextType = BookContext;
 
   render() {
 
-    const bookList = this.props.books.map((book, i) => {
-      return <Book book={book} key={i} />
-    })
+    const books = this.context
 
-    return <div>{bookList}</div>
+    return (
+      <section className="page-section bg-light" id="portfolio">
+        <div className="container">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">BookFolio</h2>
+            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          </div>
+          <div className="row">
+
+            {books.map((book, i) => {
+              return <Book book={book} key={i} />
+            })
+            }
+
+          </div>
+        </div>
+      </section>
+    )
   }
 }
 
 export default BookList;
+
+/*
+Default Value ---> this.context
+
+1.adım - context nesnesini oluşturduk.
+2.adım - bu context nesnesimize başlangıç değeri verdik ve export ettik(books yani default value)
+3.adım - Sonra kullanmak istediğimiz componentta ( BookList.js)'de import ettik.
+4.adım - static contextType = BookContext; olarak atadık.
+5.adım - this.context ile ilgili veriye ulaştık
+
+ */
